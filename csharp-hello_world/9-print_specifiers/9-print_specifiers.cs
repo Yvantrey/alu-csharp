3e095afc298eb9fe3decc8b53c9d248b461c650c
Yvantrey <rugambayvan1@gmail.com>
2025-09-13 21:03:49 +0200

    9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Globalization;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         double percent = .7553;
         double currency = 98765.4321;
         var culture = new CultureInfo("en-US");
-        Console.WriteLine("Percent: {0:P2}", percent);
-        Console.WriteLine("Currency: {0:C2}", currency.ToString("C2", culture));
+        Console.WriteLine(string.Format(culture, "Percent: {0:P2}", percent));
+        Console.WriteLine(string.Format(culture, "Currency: {0:C2}", currency));
     }
 }
