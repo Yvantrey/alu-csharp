239dc0975a25004b693aa49e000f8a45b31f763f
Yvantrey <rugambayvan1@gmail.com>
2025-09-13 22:18:31 +0200

    11

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
     {
         string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
         str = str.Replace("C#", "CSharp").Replace(", and type-safe ", " ");
-        Console.WriteLine(
+        Console.WriteLine(string.Join(" ", args));
             str.Substring(
                 str.IndexOf("object-oriented"),
                 str.IndexOf("programming") + "programming".Length - str.IndexOf("object-oriented")
