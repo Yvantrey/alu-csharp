28e5c36d093eb971855b0b1a342a92e50023898b
Yvantrey <rugambayvan1@gmail.com>
2025-09-29 16:45:38 +0200

    0

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -1,25 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class List
+class List
 {
     public static int Sum(List<int> myList)
     {
-        if (myList == null) return 0; // null-safety
+        // HashSet removes duplicates automatically
+        HashSet<int> unique = new HashSet<int>(myList);
 
-        HashSet<int> uniqueNumbers = new HashSet<int>();
         int sum = 0;
-
-        foreach (int number in myList)
-        {
-            if (!uniqueNumbers.Contains(number))
-            {
-                uniqueNumbers.Add(number);
-                sum += number;
-            }
-        }
+        foreach (int num in unique)
+            sum += num;
 
         return sum;
     }
 }
-
