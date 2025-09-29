e9bb62a7377c792d8ace92eac153a884cf204f08
Yvantrey <rugambayvan1@gmail.com>
2025-09-29 10:28:59 +0200

    14

--- csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -4,14 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] array = new int[5, 5];
+        int[,] array = new int[5, 5];  // 5x5 array initialized to 0
+
+        // Set the middle element [2,2] to 1
         array[2, 2] = 1;
 
+        // Print the array
         for (int i = 0; i < 5; i++)
         {
             for (int j = 0; j < 5; j++)
             {
-                Console.Write(array[i, j] + " ");
+                Console.Write(array[i, j]);
+                if (j < 4)  // spacing between elements
+                    Console.Write(" ");
             }
             Console.WriteLine();
         }
