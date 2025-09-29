b266aaf4f9ecd28d84d9643b99ad3a634be67880
Yvantrey <rugambayvan1@gmail.com>
2025-09-29 09:40:13 +0200

    9

---
+++ csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        // If key already exists, its value will be replaced automatically
+        myDict[key] = value;
+        return myDict;
+    }
+}
