6908d11d4227f3ab0926c4fcf0e3047dc8ff4fa8
Yvantrey <rugambayvan1@gmail.com>
2025-11-02 15:59:17 +0200

    1

--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -4,18 +4,20 @@ namespace MyMath
 {
     public class Matrix
     {
-        /// <summary>
-        /// Divides all elements of a matrix by a number.
-        /// </summary>
-        /// <param name="matrix">Input matrix</param>
-        /// <param name="num">Number to divide by</param>
-        /// <returns>New matrix with divided elements, or null if input invalid</returns>
         public static int[,] Divide(int[,] matrix, int num)
         {
-            if (matrix == null) return null;
+            // Handle null matrix
+            if (matrix == null)
+                return null;
 
             try
             {
+                if (num == 0)
+                {
+                    Console.WriteLine("Num cannot be 0");
+                    return null;
+                }
+
                 int rows = matrix.GetLength(0);
                 int cols = matrix.GetLength(1);
                 int[,] result = new int[rows, cols];
@@ -33,6 +35,10 @@ namespace MyMath
             catch (DivideByZeroException)
             {
                 Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            catch (Exception)
+            {
                 return null;
             }
         }
