bd93077b97cc572f3c359cc500bdf0861a19ab4e
Yvantrey <rugambayvan1@gmail.com>
2025-11-15 13:05:41 +0200

    24

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -14,27 +14,35 @@ public class MatrixMath
     /// </returns>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        if (matrix == null)
+            return new double[,] { { -1 } };
+
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        if (rows != cols)
             return new double[,] { { -1 } };
 
         if (direction != 'x' && direction != 'y')
             return new double[,] { { -1 } };
 
-        double[,] result = new double[2, 2];
-
+        double[,] shearMatrix;
         if (direction == 'x')
         {
-            result[0, 0] = matrix[0, 0] + factor * matrix[1, 0];
-            result[1, 0] = matrix[1, 0];
-            result[0, 1] = matrix[0, 1] + factor * matrix[1, 1];
-            result[1, 1] = matrix[1, 1];
+            shearMatrix = new double[,] { { 1, factor }, { 0, 1 } };
         }
         else
         {
-            result[0, 0] = matrix[0, 0];
-            result[1, 0] = matrix[1, 0] + factor * matrix[0, 0];
-            result[0, 1] = matrix[0, 1];
-            result[1, 1] = matrix[1, 1] + factor * matrix[0, 1];
+            shearMatrix = new double[,] { { 1, 0 }, { factor, 1 } };
+        }
+
+        double[,] result = new double[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = shearMatrix[i, 0] * matrix[0, j] + shearMatrix[i, 1] * matrix[1, j];
+            }
         }
 
         return result;
