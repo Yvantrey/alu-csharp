a272c464aa85b0fdabce79b1f0f7d42bfb06e867
Yvantrey <rugambayvan1@gmail.com>
2025-11-15 12:58:30 +0200

    Implement matrix shear 2D transformation

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -14,35 +14,27 @@ public class MatrixMath
     /// </returns>
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        if (matrix == null)
-            return new double[,] { { -1 } };
-
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-
-        if (rows != cols)
+        if (matrix == null || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] { { -1 } };
 
         if (direction != 'x' && direction != 'y')
             return new double[,] { { -1 } };
 
-        double[,] result = new double[rows, cols];
+        double[,] result = new double[2, 2];
 
-        for (int i = 0; i < rows; i++)
+        if (direction == 'x')
         {
-            for (int j = 0; j < cols; j++)
-            {
-                if (direction == 'x')
-                {
-                    // Shear in X direction: newValue = original + factor * rowIndex
-                    result[i, j] = matrix[i, j] + factor * i;
-                }
-                else // direction == 'y'
-                {
-                    // Shear in Y direction: newValue = original + factor * colIndex
-                    result[i, j] = matrix[i, j] + factor * j;
-                }
-            }
+            result[0, 0] = matrix[0, 0] + factor * matrix[1, 0];
+            result[1, 0] = matrix[1, 0];
+            result[0, 1] = matrix[0, 1] + factor * matrix[1, 1];
+            result[1, 1] = matrix[1, 1];
+        }
+        else
+        {
+            result[0, 0] = matrix[0, 0];
+            result[1, 0] = matrix[1, 0] + factor * matrix[0, 0];
+            result[0, 1] = matrix[0, 1];
+            result[1, 1] = matrix[1, 1] + factor * matrix[0, 1];
         }
 
         return result;
